30ce006a4b081992aaeb341668503bd6485bfd27
Ankesh04 <osankeshagarwal4@gmail.com>
2026-04-11 21:52:47 +0530

    npm continue

---
+++ npm.js
@@ -0,0 +1,22 @@
+// NPM(Node Package Manager)
+// it is the standard package manager for Node.js
+
+// npm is a library of packages
+
+// command line tool- we can install the package to use tht package
+// Installing Packages
+// npm install package_name
+
+// when we install packages then are several files are been downloaded:
+
+// node_modules
+// this folder contains every installed dependency for your projects
+
+// pckage-lock.json
+// it records the exact version of every installed dependency,
+// including its sub-dependecnies and their version.
+
+//if we have to use the package then we have to make a index.js file
+
+// package.json
+// .....
